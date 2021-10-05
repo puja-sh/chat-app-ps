fb5bbba809b0ba68cc617ca937643d816ccf72f3
puja <pujashm6@gmail.com>
2021-10-05 15:26:37 +0530

    initial sidebar

---
+++ src/components/dashboard/DashboardToggle.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, Drawer, Icon } from 'rsuite';
+import { useModalState } from '../../misc/custom-hooks';
+import Dashboard from './Index';
+
+const DashboardToggle = () => {
+  const { isOpen, open, close } = useModalState();
+
+  return (
+    <>
+      <Button block color="blue" onClick={open}>
+        <Icon icon="dashboard" /> Dashboard
+      </Button>
+      <Drawer show={isOpen} onHide={close} placement="left">
+        <Dashboard />
+      </Drawer>
+    </>
+  );
+};
+
+export default DashboardToggle;
---
+++ src/components/dashboard/Index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return <div>hello index page</div>;
+};
+
+export default Dashboard;
