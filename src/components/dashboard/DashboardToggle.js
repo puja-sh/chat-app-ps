4ce5648aa1065f4e8cb99df2294b7f9337255355
puja <pujashm6@gmail.com>
2021-10-05 16:00:53 +0530

    sidebar: signout functionality, display user name

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import { Button, Drawer, Icon } from 'rsuite';
+import { useCallback } from 'react/cjs/react.development';
+import { Alert, Button, Drawer, Icon } from 'rsuite';
 import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
+import { auth } from '../../misc/firebase';
 import Dashboard from './Index';
 
 const DashboardToggle = () => {
   const { isOpen, open, close } = useModalState();
   const isMobile = useMediaQuery('(max-width: 992px)');
 
+  const onSignOutHandler = useCallback(() => {
+    auth.signOut();
+
+    Alert.info('Sign Out');
+
+    close();
+  }, [close]);
   return (
     <>
       <Button block color="blue" onClick={open}>
         <Icon icon="dashboard" /> Dashboard
       </Button>
       <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
-        <Dashboard />
+        <Dashboard onSignOutHandler={onSignOutHandler} />
       </Drawer>
     </>
   );
