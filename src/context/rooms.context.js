f3fb6b9411daf023d3b8f6f8ef036974585a1bb3
puja <pujashm6@gmail.com>
2021-10-06 12:31:21 +0530

    chat-room: created room context

---
+++ src/context/rooms.context.js
@@ -0,0 +1,24 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { database } from '../misc/firebase';
+import { transformIntoArray } from '../misc/helper';
+
+const RoomsContext = createContext();
+
+export const RoomsProvider = ({ children }) => {
+  const [rooms, setRooms] = useState();
+
+  useEffect(() => {
+    const roomListRef = database.ref(`rooms`);
+
+    roomListRef.on('value', snap => {
+      const data = transformIntoArray(snap.val());
+      setRooms(data);
+    });
+    return () => {
+      roomListRef.off();
+    };
+  }, []);
+  return (
+    <RoomsContext.Provider value={rooms}> {children} </RoomsContext.Provider>
+  );
+};
--- src/misc/helper.js
+++ src/misc/helper.js
@@ -1,3 +1,4 @@
+// Initial Names
 export function getNameInitials(name) {
   const splitname = name.toUpperCase().split(' ');
 
@@ -8,4 +9,13 @@ export function getNameInitials(name) {
 
   // if name word == 1
   return splitname[0][0];
+}
+
+// Transform it into an Array
+export function transformIntoArray(snapVal) {
+  return snapVal
+    ? Object.keys(snapVal).map(roomId => {
+        return { ...snapVal[roomId], id: roomId };
+      })
+    : [];
 }
