448ece36247b624d6fc5c3170c13315ef0e952f0
puja <pujashm6@gmail.com>
2021-10-12 23:43:30 +0530

    chat: correction in chat layout

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -18,7 +18,9 @@ const Sidebar = () => {
       <div ref={topSidebarRef}>
         <DashboardToggle />
         <CreateRoomBtnModal />
-        <Divider>Join conversation</Divider>
+        <Divider style={{ margin: 0, padding: '30px 0' }}>
+          Join conversation
+        </Divider>
       </div>
       <ChatRoomList aboveHeight={height} />
     </div>
--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import { Loader } from 'rsuite';
+
+import ChatTop from '../../components/chat-window/top/Index';
+import ChatBottom from '../../components/chat-window/bottom/Index';
+import Messages from '../../components/chat-window/messages/Index';
+import { useRooms } from '../../context/rooms.context';
 
 const Chat = () => {
-  return <div>hello there chat</div>;
+  const { chatId } = useParams(); // chat id from the route
+
+  const rooms = useRooms(); // rooms data from useRooms Context
+
+  if (!rooms) {
+    // if not rooms
+    return <Loader center vertical size="md" content="Loading" speed="slow" />;
+  }
+
+  const currentRoom = rooms.find(room => room.id === chatId); // if roomid == route id then currentRoom
+
+  // If chat room not correct
+  if (!currentRoom) {
+    return <h6 className="text-center mt-page"> Chat {chatId} not found!</h6>;
+  }
+
+  return (
+    <>
+      <div className="chat-top">
+        <ChatTop />
+      </div>
+      <div className="chat-middle">
+        <Messages />
+      </div>
+      <div className="chat-bottom">
+        <ChatBottom />
+      </div>
+    </>
+  );
 };
 
 export default Chat;
