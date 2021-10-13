21850026d8839f3fddd406402c448c74eee421fd
puja <pujashm6@gmail.com>
2021-10-14 02:02:20 +0530

    chat-bottom: created chat bottom; send messages in the chat

--- src/components/chat-window/bottom/Index.js
+++ src/components/chat-window/bottom/Index.js
@@ -1,7 +1,89 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { useParams } from 'react-router';
+import { Alert, Icon, Input, InputGroup } from 'rsuite';
+import firebase from 'firebase/app';
+import { useProfile } from '../../../context/profile.context';
+import { database } from '../../../misc/firebase';
+
+function assembleMessage(profile, chatId) {
+  return {
+    roomId: chatId,
+    author: {
+      name: profile.name,
+      uid: profile.uid,
+      createdAt: profile.createdAt,
+      ...(profile.avatar ? { avatar: profile.avatar } : {}),
+    },
+    createdAt: firebase.database.ServerValue.TIMESTAMP,
+  };
+}
 
 const Bottom = () => {
-  return <div>hh</div>;
+  const [input, setInput] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const { chatId } = useParams();
+  const { profile } = useProfile();
+
+  const onInputChangeHandler = useCallback(value => {
+    setInput(value);
+  }, []);
+
+  const onSendClickHandler = async () => {
+    if (input.trim === '') {
+      return;
+    }
+
+    const msgData = assembleMessage(profile, chatId);
+    msgData.text = input;
+
+    const updates = {};
+
+    const messageId = database.ref('messages').push().key; // we will get unique key in real time database
+
+    updates[`/messages/${messageId}`] = msgData;
+    updates[`/rooms/${chatId}/lastMessage`] = {
+      ...msgData,
+      msgId: messageId,
+    };
+
+    setIsLoading(true);
+    try {
+      await database.ref().update(updates);
+
+      setInput('');
+    } catch (error) {
+      setIsLoading(false);
+      Alert.error(error.message, 10000);
+    }
+  };
+  const onKeyDownHandler = ev => {
+    if (ev.keyCode === 13) {
+      ev.preventDefault();
+      onSendClickHandler();
+    }
+  };
+
+  return (
+    <div>
+      <InputGroup>
+        <Input
+          type="text"
+          placeholder="Message"
+          value={input}
+          onChange={onInputChangeHandler}
+          onKeyDown={onKeyDownHandler}
+        />
+        <InputGroup.Button
+          color="blue"
+          appearance="primary"
+          onClick={onSendClickHandler}
+          disabled={isLoading}
+        >
+          <Icon icon="send" />
+        </InputGroup.Button>
+      </InputGroup>
+    </div>
+  );
 };
 
 export default Bottom;
