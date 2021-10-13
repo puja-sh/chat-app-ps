70343a3024888a54c0905c6b9219355c77802ad5
puja <pujashm6@gmail.com>
2021-10-14 03:09:27 +0530

    update all user reference if data is updated

--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -5,6 +5,7 @@ import { useModalState } from '../../misc/custom-hooks';
 import { database, storage } from '../../misc/firebase';
 import { useProfile } from '../../context/profile.context';
 import ProfileAvatar from './ProfileAvatar';
+import { getUserUpdates } from '../../misc/helper';
 
 const fileTypeInput = '.jpeg, .jpg, .png';
 
@@ -68,12 +69,20 @@ const AvatarUploadBtn = () => {
       const downloadUrl = await uploadAvatarResult.ref.getDownloadURL();
 
       // Uploading in database
-      const uesrAvatarRef = database
-        .ref(`/profiles/${profile.uid}`)
-        .child('avatar');
+      // const uesrAvatarRef = database
+      //   .ref(`/profiles/${profile.uid}`)
+      //   .child('avatar');
+
+      const updates = await getUserUpdates(
+        profile.uid,
+        'avatar',
+        downloadUrl,
+        database
+      );
+      await database.ref().update(updates);
 
       // Upload
-      uesrAvatarRef.set(downloadUrl);
+      // uesrAvatarRef.set(downloadUrl);
 
       setIsLoading(false); // Uplaoding done to stop the loading
       close();
--- src/components/dashboard/Index.js
+++ src/components/dashboard/Index.js
@@ -2,19 +2,31 @@ import React from 'react';
 import { Alert, Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
 import { database } from '../../misc/firebase';
+import { getUserUpdates } from '../../misc/helper';
 import EditableInput from '../EditableInput';
 import AvatarUploadBtn from './AvatarUploadBtn';
 import ProviderBlock from './ProviderBlock';
 
 const Dashboard = ({ onSignOutHandler }) => {
   const { profile } = useProfile();
+
   const onSaveHandler = async newData => {
-    const userNicknameRef = database
-      .ref(`profiles/${profile.uid}`)
-      .child('name');
+    // const userNicknameRef = database
+    //   .ref(`profiles/${profile.uid}`)
+    //   .child('name');
 
     try {
-      await userNicknameRef.set(newData); // update child 'name' with new Data
+      // await userNicknameRef.set(newData); // update child 'name' with new Data
+
+      const updates = await getUserUpdates(
+        profile.uid,
+        'name',
+        newData,
+        database
+      );
+
+      await database.ref().update(updates);
+
       Alert.info('Nickname has been updated', 4000);
     } catch (error) {
       Alert.error(error.message, 4000);
