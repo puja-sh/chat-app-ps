cbabd667a8ab9cdcdda3388e812042272c48d325
puja <pujashm6@gmail.com>
2021-10-14 22:09:17 +0530

    created icon btn control comp (like)

---
+++ src/components/chat-window/messages/IconBtnControl.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Badge, Icon, IconButton, Tooltip, Whisper } from 'rsuite';
+
+const ConditionBadge = ({ condition, children }) => {
+  return condition ? <Badge content={condition}> {children} </Badge> : children;
+};
+const IconBtnControl = ({
+  isVisible,
+  iconName,
+  tooltip,
+  onClick,
+  badgeContent,
+  ...props
+}) => {
+  return (
+    <div
+      className="ml-2"
+      style={{ visibility: isVisible ? 'visible' : 'hidden' }}
+    >
+      <ConditionBadge condition={badgeContent}>
+        <Whisper
+          placement="top"
+          deplay={0}
+          deplayHide={0}
+          deplayShow={0}
+          trigger="hover"
+          speaker={<Tooltip> {tooltip} </Tooltip>}
+        >
+          <IconButton
+            {...props}
+            onClick={onClick}
+            circle
+            size="xs"
+            icon={<Icon icon={iconName} />}
+          />
+        </Whisper>
+      </ConditionBadge>
+    </div>
+  );
+};
+
+export default IconBtnControl;
--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -6,6 +6,7 @@ import { useHover } from '../../../misc/custom-hooks';
 import { auth } from '../../../misc/firebase';
 import ProfileAvatar from '../../dashboard/ProfileAvatar';
 import PresenceDot from '../../PresenceDot';
+import IconBtnControl from './IconBtnControl';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
 const MessageItem = ({ message, handleAdmin }) => {
@@ -50,6 +51,14 @@ const MessageItem = ({ message, handleAdmin }) => {
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
+        />
+        <IconBtnControl
+          {...(true ? { color: 'red' } : {})}
+          isVisible
+          iconName="heart"
+          tooltip="Like this message"
+          onClick={() => {}}
+          badgeContent={5}
         />
       </div>
       <div>
