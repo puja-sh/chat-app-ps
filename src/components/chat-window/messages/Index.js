77fad9a17454f6048378e504e070a6f5b6a27519
puja <pujashm6@gmail.com>
2021-10-15 02:36:10 +0530

    grouped msgs by dates

--- src/components/chat-window/messages/Index.js
+++ src/components/chat-window/messages/Index.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { useParams } from 'react-router';
 import { Alert } from 'rsuite';
-import { transformIntoArray } from '../../../misc/helper';
+import { groupBy, transformIntoArray } from '../../../misc/helper';
 import MessageItem from './MessageItem';
 import { auth, database, storage } from '../../../misc/firebase';
 
@@ -126,19 +126,38 @@ const Messages = () => {
     [chatId, messages]
   );
 
+  const renderMessages = () => {
+    const groups = groupBy(messages, item =>
+      new Date(item.createdAt).toDateString()
+    );
+
+    const items = [];
+
+    Object.keys(groups).forEach(date => {
+      items.push(
+        <li key={date} className="text-center mb-1 padded">
+          {date}{' '}
+        </li>
+      );
+
+      const msgs = groups[date].map(msg => (
+        <MessageItem
+          key={msg.id}
+          message={msg}
+          handleAdmin={handleAdmin}
+          handleLike={handleLike}
+          handleDelete={handleDelete}
+        />
+      ));
+
+      items.push(...msgs);
+    });
+    return items;
+  };
   return (
     <ul className="msg-list custom-scroll">
       {isChatEmpty && <li>No Messages yet</li>}
-      {canShowMessages &&
-        messages.map(msg => (
-          <MessageItem
-            key={msg.id}
-            message={msg}
-            handleAdmin={handleAdmin}
-            handleLike={handleLike}
-            handleDelete={handleDelete}
-          />
-        ))}
+      {canShowMessages && renderMessages()}
     </ul>
   );
 };
--- src/misc/helper.js
+++ src/misc/helper.js
@@ -54,3 +54,18 @@ export async function getUserUpdates(userId, keyToUpdate, value, db) {
 export function transformToArray(snapVal) {
   return snapVal ? Object.keys(snapVal) : [];
 }
+
+// Grouping
+export function groupBy(array, groupingKeyFn) {
+  return array.reduce((result, item) => {
+    const groupingKey = groupingKeyFn(item);
+
+    if (!result[groupingKey]) {
+      result[groupingKey] = [];
+    }
+
+    result[groupingKey].push(item);
+
+    return result;
+  }, {});
+}
